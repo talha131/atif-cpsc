226361892792f48d55c78fa5e6bbeadadf73110e
MADEEB80 <ranatechpak@gmail.com>
2024-12-17 22:25:23 +0500

    FIx Mobile view for nav

--- Scripts.js
+++ Scripts.js
@@ -12,7 +12,6 @@ window.addEventListener('scroll', () => {
         }
     });
 });
-
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.getElementById('hamburger');
     const navLinks = document.getElementById('nav-links');
